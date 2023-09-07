427256557422b9fbf10ce43d4d6c5e29cc59987e
Thomaz Botelho <thomazrb@gmail.com>
2023-09-07 17:53:16 -0300

    Preparando para pegar os valores

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:exemplo_flutter_form_validator/components/my_form.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
